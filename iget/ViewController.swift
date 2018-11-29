d055dd27a3c36dda5d8b2ab7149308030b8784a1
alfredking <alfredking@126.com>
2018-11-29 22:46:59 +0800

    swift array三种类型

--- iget/ViewController.swift
+++ iget/ViewController.swift
@@ -13,11 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //在 Arrays.swift 中,主要定义了 Array, ContiguousArray 和ArraySlice 三种public 类型,它们具有相同的接口,而且结构和行为也很类似
         let say="hello swift"
         print(say)
         let nums=[1,2,3]
+        print(nums)
         let nums1=[Int](repeating:0,count:5)
+        print(nums1)
         var nums2=[3,1,2]
+        print(nums2)
+        nums2.append(4)
+        print(nums2)
+        
+        nums2.sort()
+        print(nums2)
+        
+        nums2.sort(by: >)
+        print(nums2)
+        
+        let anotherNums=Array(nums2[0..<nums2.count-1])
+        print(anotherNums)
         
     }
 
